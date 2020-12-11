e8925f3eaa74fe28470a9ee71966daa8f90fd412
Marcin Kaminski <marcin.kaminski@schibsted.com>
2020-12-11 17:11:52 +0100

    ✨ Displayed auction name and added url

--- src/database/models/auction.js
+++ src/database/models/auction.js
@@ -6,6 +6,7 @@ const AuctionSchema = new mongoose.Schema(
     active: { type: Boolean, default: false },
     price: { type: Number, min: 0, default: 0 },
     currency: { type: String, default: 'PLN' },
+    url: { type: String, default: '#' },
     winnerEmail: { type: String },
     description: { type: String },
     avatarUrl: { type: String },
